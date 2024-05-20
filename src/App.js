47f42acf8fdb5cc67eb2567df0c5410714bc215b
malyFertig <malyFertig@gmail.com>
2024-05-20 13:43:04 +0300

    move navbar to neww component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import  Login  from './components/Login/Login';
 import SignUp from './components/SingUp/SignUp'
 import Home from './components/Home/Home'
+import Navbar from './components/Home/Navbar';
 
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
      <Routes>
-      <Route path='' element={<Home/>}/>
+      <Route path='' element={<Navbar title="Cleaning Lady"/>}/>
       <Route path='Login' element={<Login/>}/>
       <Route path='home' element={<Home/>}/>
       <Route path='SignUp' element={<SignUp/>}/>
