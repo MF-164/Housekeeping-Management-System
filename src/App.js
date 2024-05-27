eecf25d2d548ed859c696198df26e7f841bde38f
malyFertig <malyFertig@gmail.com>
2024-05-27 14:53:53 +0300

    move computer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
+import Home from './Home';
 import CleaningLady from './store/features/CleaningLady/CleaningLady';
-import Client from './store/features/Client/Client';
-import ClientList from './store/features/Client/ClientList';
+
 function App() {
-  const client ={Id:0,Name:'Brayndy',Password:1234,Phone:'0534122757',City:'Elad',Address:'Ben zakay',HouseNumber:9}
+ 
   const cleaningLady={Id:22,FirstName:'Alona',LastName:'Yev',Phone:'0534122757',OriginCountry:'Tailand',City:'Ramat Gan',Address:'yona',HouseNumber:20,Status:'married',HourlyPrice:120}
   return (
     <div className="App">
-     {/* <Client client={client}></Client> */}
-     {/* <ClientList></ClientList> */}
      <CleaningLady cleaningLady={cleaningLady}></CleaningLady>
+     <Home/>
     </div>
   );
 }
