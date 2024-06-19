650735769c9901164792eb931600bf642c4385d9
malyFertig <malyFertig@gmail.com>
2024-06-19 19:34:08 +0300

    move to braindy

--- src/store/features/Comment/CommentList.jsx
+++ src/store/features/Comment/CommentList.jsx
@@ -4,24 +4,24 @@ import { fetchAllCommentsFromServer } from "./commentSlice"
 import Comment from "./Comment"
 import Navbar from "../../../components/Navbar/Navbar"
 import './CommentList.scss'
-
-
+import './AddComment'
 
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import CssBaseline from '@mui/material/CssBaseline';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import AddComment from "./AddComment"
+
+
 
 function ElevationScroll(props) {
+
     const { children, window } = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
+
     const trigger = useScrollTrigger({
         disableHysteresis: true,
         threshold: 0,
@@ -35,12 +35,11 @@ function ElevationScroll(props) {
 
 ElevationScroll.propTypes = {
     children: PropTypes.element.isRequired,
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
     window: PropTypes.func,
 };
+
+
+
 
 
 
@@ -113,6 +112,7 @@ const CommentList = () => {
                 </Box>
             </Container>
         </React.Fragment>
+        <AddComment cleaningLady={cleaningLady} />
     </div>)
 }
 export default CommentList
